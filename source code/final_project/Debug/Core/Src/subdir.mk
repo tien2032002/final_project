aabfeb8c398fe5b4dc1fa425b4066cecc415a7ee
I2Relax <minhquan7102@gmail.com>
2022-12-13 16:30:24 +0700

    Revert "Quan init"
    This reverts commit df6b48a636c9889cdbb68b5157340f72f01f31f9.

--- source code/final_project/Debug/Core/Src/subdir.mk
+++ source code/final_project/Debug/Core/Src/subdir.mk
@@ -1,11 +1,10 @@
 ################################################################################
 # Automatically-generated file. Do not edit!
-# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Core/Src/fsm_pedestrian.c \
 ../Core/Src/global.c \
 ../Core/Src/hardware_layer.c \
 ../Core/Src/input_reading.c \
@@ -18,7 +17,6 @@ C_SRCS += \
 ../Core/Src/system_stm32f1xx.c 
 
 OBJS += \
-./Core/Src/fsm_pedestrian.o \
 ./Core/Src/global.o \
 ./Core/Src/hardware_layer.o \
 ./Core/Src/input_reading.o \
@@ -31,7 +29,6 @@ OBJS += \
 ./Core/Src/system_stm32f1xx.o 
 
 C_DEPS += \
-./Core/Src/fsm_pedestrian.d \
 ./Core/Src/global.d \
 ./Core/Src/hardware_layer.d \
 ./Core/Src/input_reading.d \
@@ -45,6 +42,13 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Core/Src/%.o: ../Core/Src/%.c Core/Src/subdir.mk
+Core/Src/%.o Core/Src/%.su: ../Core/Src/%.c Core/Src/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../Core/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-Core-2f-Src
+
+clean-Core-2f-Src:
+	-$(RM) ./Core/Src/global.d ./Core/Src/global.o ./Core/Src/global.su ./Core/Src/hardware_layer.d ./Core/Src/hardware_layer.o ./Core/Src/hardware_layer.su ./Core/Src/input_reading.d ./Core/Src/input_reading.o ./Core/Src/input_reading.su ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/software_timer.d ./Core/Src/software_timer.o ./Core/Src/software_timer.su ./Core/Src/stm32f1xx_hal_msp.d ./Core/Src/stm32f1xx_hal_msp.o ./Core/Src/stm32f1xx_hal_msp.su ./Core/Src/stm32f1xx_it.d ./Core/Src/stm32f1xx_it.o ./Core/Src/stm32f1xx_it.su ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32f1xx.d ./Core/Src/system_stm32f1xx.o ./Core/Src/system_stm32f1xx.su
+
+.PHONY: clean-Core-2f-Src
 
